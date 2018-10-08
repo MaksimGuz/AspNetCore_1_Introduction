f0c7cc00ba7165585b738ffc0b7e0e121f94b2b4
Maksim Guz <Maksim_Guz@epam.com>
2018-10-08 18:15:51 +0300

    Module 2. Task 1. Create tests for CategoriesController

---
+++ BaseSiteWebApp/XUnitTestProject/CategoriesControllerTest.cs
@@ -0,0 +1,92 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using Moq;
+using BaseSiteWebApp.Controllers;
+using BaseSiteWebApp.Interfaces;
+using BaseSiteWebApp.Models;
+using BaseSiteWebApp.ViewModels;
+using System;
+
+namespace XUnitTestProject
+{
+    public class CategoriesControllerTest
+    {
+        [Fact]
+        public async Task Index_ReturnsAViewResult_WithAListOfCategories()
+        {
+            // Arrange
+            var mockService = new Mock<ICategoriesService>();
+            mockService.Setup(srv => srv.GetAllAsync()).ReturnsAsync(GetTestCategories());
+            var controller = new CategoriesController(mockService.Object);
+
+            // Act
+            var result = await controller.Index();
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<CategoriesIndexViewModel>(viewResult.ViewData.Model);
+            Assert.NotNull(model.Categories);
+            Assert.Equal(2, model.Categories.Count());
+        }
+
+        [Fact]
+        public async Task Details_ThrowsException_WithNullId()
+        {
+            // Arrange
+            var mockService = new Mock<ICategoriesService>();
+            mockService.Setup(srv => srv.GetAllAsync()).ReturnsAsync(GetTestCategories());
+            var controller = new CategoriesController(mockService.Object);
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(() => controller.Details(null));
+        }
+
+        [Fact]
+        public async Task Details_ReturnCategoryWithExistingId()
+        {
+            // Arrange
+            var mockService = new Mock<ICategoriesService>();
+            mockService.Setup(srv => srv.GetByIdAsync(1)).ReturnsAsync(new Categories() { CategoryId = 1 });
+            var controller = new CategoriesController(mockService.Object);
+
+            // Act
+            var result = await controller.Details(1);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<Categories>(viewResult.ViewData.Model);
+            Assert.NotNull(model);
+            Assert.Equal(1, model.CategoryId);
+        }
+
+        [Fact]
+        public async Task Details__ReturnsHttpNotFound_ForInvalidId()
+        {
+            int id = 2;
+            // Arrange
+            var mockService = new Mock<ICategoriesService>();
+            mockService.Setup(srv => srv.GetByIdAsync(1)).ReturnsAsync(new Categories() { CategoryId = 1 });
+            var controller = new CategoriesController(mockService.Object);
+
+            // Act
+            var result = await controller.Details(id);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+        }
+
+        private CategoriesIndexViewModel GetTestCategories()
+        {
+            return new CategoriesIndexViewModel {
+                Categories = new List<Categories>() {
+                    new Categories() { CategoryId = 1 },
+                    new Categories() { CategoryId = 2 }
+                }
+            };
+        }
+    }
+}
